9b7d1cba9841111b7cae4b8c042751750998aefc
Mohan <mohankandregula1@gmail.com>
2026-01-13 18:59:15 +0530

    added variable file

---
+++ variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#to add variable mention var = content
+#to execute the variable mention $variable ot ${variable}
+
+
+person1= 'mohan'
+person2= 'ramesh'
+
+echo '${person1} and ${person2} are friends'
