d5842d85452d4c01124708386d485f4a7a894531
Mohan <mohankandregula1@gmail.com>
2026-03-09 20:12:02 +0530

    added some basic notes

--- note.sh
+++ note.sh
@@ -3,3 +3,24 @@
 * the linux adminstration.
 
 * git 
+
+
+#!/bin/bash  -> is called as shebang and it should be the firstline and the interpret to check the syntax of the cript and execute them.
+
+sh == bash
+.sh is the extension. 
+
+echo "hello world"
+
+# == comment, is to provide the info. for the script 
+
+
+to execute the file or to run the file. use
+
+sh <fileName> or 
+
+bash <fileName>  or 
+
+./<fileName>  # If you have the executable access.
+
+
