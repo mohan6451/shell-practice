a3fdd98cc253597bd439e84292ee06ca808f089b
Mohan <mohankandregula1@gmail.com>
2026-03-11 16:02:33 +0530

    notes

--- 10_color_code.sh
+++ 10_color_code.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-R= '\e[31m' #Red
-G= '\e[32m' #Green
-y= '\e[33m' #Yellow
-N= '\e[0m' #No Color
+R= "\e[31m" #Red
+G= "\e[32m" #Green
+y= "\e[33m" #Yellow
+N= "\e[0m" #No Color
 
 
 echo -e "$G   Hi Mohan!! $N"
